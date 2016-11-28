14aa6a1bdd3f9733263cfab85ca5292de2e7563f
522d <kokororok_15@yahoo.com>
2016-11-28 15:47:51 +0800

    'Webhook'

--- routes/index.js
+++ routes/index.js
@@ -25,6 +25,62 @@ router.get('/tables', function(req, res, next) {
 });
 
 
+app.get('/webhook/', function (req, res) {
+    if (req.query['hub.verify_token'] === 'Aha_Moment_Labs') {
+        res.send(req.query['hub.challenge'])
+    }
+    res.send('Error, wrong token')
+})
+
+
+app.post('/webhook/', function (req, res) {
+    messaging_events = req.body.entry[0].messaging
+    for (i = 0; i < messaging_events.length; i++) {
+        event = req.body.entry[0].messaging[i]
+        sender = event.sender.id
+        if (event.message && event.message.text) {
+            text = event.message.text
+            if (text === 'hi') {
+                sendGenericMessage(sender)
+                continue
+            }
+            sendTextMessage(sender, "parrot: " + text.substring(0, 200))
+        }
+        if (event.postback) {
+            text = JSON.stringify(event.postback)
+            sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token)
+            continue
+        }
+    }
+    res.sendStatus(200)
+})
+
+var token = " enter token here"
+
+// function to echo back messages - added by Stefan
+
+function sendTextMessage(sender, text) {
+    messageData = {
+        text:text
+    }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
+
+
 
 /*
 router.post('/tables/getTableLists', function(req, res, next) {
